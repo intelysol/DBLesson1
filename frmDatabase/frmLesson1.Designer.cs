bc127ee2d8b0389d12e85024db870920b732ebd6
Shahid Mehmood <iss_shahid@yahoo.com>
2019-10-03 18:17:03 +0500

    Frist database lesson

---
+++ frmDatabase/frmLesson1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace frmDatabase
+{
+    partial class frmLesson1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtDptName = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbDepartments = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 14);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "DptName:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(117, 66);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(66, 22);
+            this.txtId.TabIndex = 1;
+            this.txtId.Leave += new System.EventHandler(this.txtId_Leave);
+            // 
+            // txtDptName
+            // 
+            this.txtDptName.Location = new System.Drawing.Point(117, 97);
+            this.txtDptName.Name = "txtDptName";
+            this.txtDptName.Size = new System.Drawing.Size(286, 22);
+            this.txtDptName.TabIndex = 1;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(50, 156);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 34);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "&New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(131, 156);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 34);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(212, 156);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 34);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(293, 156);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 34);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label3.Font = new System.Drawing.Font("Garamond", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(-3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(451, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Department Setup";
+            // 
+            // cmbDepartments
+            // 
+            this.cmbDepartments.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDepartments.FormattingEnabled = true;
+            this.cmbDepartments.Location = new System.Drawing.Point(189, 69);
+            this.cmbDepartments.Name = "cmbDepartments";
+            this.cmbDepartments.Size = new System.Drawing.Size(214, 22);
+            this.cmbDepartments.TabIndex = 3;
+            this.cmbDepartments.SelectedIndexChanged += new System.EventHandler(this.cmbDepartments_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 208);
+            this.Controls.Add(this.cmbDepartments);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtDptName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Garamond", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtDptName;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbDepartments;
+    }
+}
+
---
+++ frmDatabase/frmLesson1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmDatabase
+{
+    public partial class frmLesson1 : Form
+    {
+        SqlConnection Conn=new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\Administrator\documents\visual studio 2013\Projects\frmDatabase\frmDatabase\Database.mdf';Integrated Security=True");
+        SqlCommand cmd;
+        string sSQL = "";
+
+        public frmLesson1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillcmbDepartments();
+            txtId.Clear();
+            txtDptName.Clear();
+
+            txtId.Focus();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            FillcmbDepartments();
+            txtId.Clear();
+            txtDptName.Clear();
+            btnSave.Text = "&Save";
+            txtId.Focus();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(txtId.Text.Length==0)
+            {
+                MessageBox.Show("Please enter the id", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtId.Focus();
+                return;
+            }
+
+            if (txtDptName.Text.Length==0)
+            {
+                MessageBox.Show("Please enter the Department Name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtDptName.Focus();
+                return;                
+            }
+
+            if (btnSave.Text =="&Save")
+            {
+
+                sSQL = "Insert into Departments(id,DeptName) values(@id,@DeptName)";
+            }
+            else
+            {
+                sSQL = "Update Departments Set DeptName=@DeptName Where id=@id";
+            }
+
+
+            if (Conn.State == ConnectionState.Closed) { Conn.Open(); }
+
+            cmd = new SqlCommand(sSQL, Conn);
+            cmd.Parameters.AddWithValue("@id", txtId.Text);
+            cmd.Parameters.AddWithValue("@DeptName", txtDptName.Text);
+            cmd.ExecuteNonQuery();
+
+            btnNew_Click(sender, e);
+
+            MessageBox.Show("Data Sucessfully Saved");
+            
+        }
+
+        private void txtId_Leave(object sender, EventArgs e)
+        {
+            if (txtId.Text.Length == 0)
+                return;
+
+            sSQL = "Select id,DeptName from Departments where id=@id";
+            if (Conn.State == ConnectionState.Closed) { Conn.Open(); }
+            cmd = new SqlCommand(sSQL, Conn);
+            cmd.Parameters.AddWithValue("@id", txtId.Text);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            btnSave.Text = "&Save";
+            if(dr.HasRows)
+            {
+                dr.Read();
+                btnSave.Text = "&Update";
+                txtDptName.Text = dr["DeptName"].ToString();
+            }
+
+            dr.Close();
+        }
+
+        private void FillcmbDepartments()
+        {
+            sSQL = "Select id,DeptName from Departments";
+            if (Conn.State == ConnectionState.Closed) { Conn.Open(); }
+
+            SqlDataAdapter da = new SqlDataAdapter(sSQL, Conn);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            cmbDepartments.DisplayMember = "DeptName";
+            cmbDepartments.ValueMember = "id";
+            cmbDepartments.DataSource = dt;
+        }
+
+        private void cmbDepartments_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtId.Text = cmbDepartments.SelectedValue.ToString();
+            txtId_Leave(sender, e);
+        }
+    }
+}
